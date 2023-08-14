81a6de6328c6a018c87fa3e961f08b75255f5546
YuvalShaffir <34415892+YuvalShaffir@users.noreply.github.com>
2023-08-14 21:51:38 +0300

    Changes: -Except nessecery loop errors. - Testing of folder importation:   * test_illegal_file_inside_folder: AssertionError: UnidentifiedImageError not raised

--- src/ImProUtils/image/image.py
+++ src/ImProUtils/image/image.py
@@ -13,6 +13,28 @@ Yuval Shaffir
 Date:
 -----
 13/08/2023
+
+Accepted file types:
+-------------------
+BLP, BMP, BUFR, CUR, DCX, DDS, DIB, EPS, FITS, FLI, FTEX, GBR, GIF, GRIB,
+HDF5, ICNS, ICO, IM, IMT, IPTC, JPEG, JPEG2000, MCIDAS, MPEG, MSP, PCD, PCX,
+PIXAR, PNG, PPM, PSD, QOI, SGI, SPIDER, SUN, TGA, TIFF, WEBP, WMF, XBM, XPM, XVTHUMB
+
+Accepted modes:
+---------------
+1 (1-bit pixels, black and white, stored with one pixel per byte)
+L (8-bit pixels, grayscale)
+P (8-bit pixels, mapped to any other mode using a color palette)
+RGB (3x8-bit pixels, true color)
+RGBA (4x8-bit pixels, true color with transparency mask)
+CMYK (4x8-bit pixels, color separation)
+YCbCr (3x8-bit pixels, color video format)
+Note that this refers to the JPEG, and not the ITU-R BT.2020, standard
+LAB (3x8-bit pixels, the L*a*b color space)
+HSV (3x8-bit pixels, Hue, Saturation, Value color space)
+Hue’s range of 0-255 is a scaled version of 0 degrees <= Hue < 360 degrees
+I (32-bit signed integer pixels)
+F (32-bit floating point pixels)
 """
 
 # Imports
@@ -28,27 +50,10 @@ def image_from_file(path, mode='RGB', err_raise=True, print_info=True):
     Import an image from a file.
     Using PIL.Image to import the image, which is considered the fastest library for that.
 
-    :param path: image of format: BLP, BMP, BUFR, CUR, DCX, DDS, DIB, EPS, FITS, FLI, FTEX, GBR, GIF, GRIB,
-                                  HDF5, ICNS, ICO, IM, IMT, IPTC, JPEG, JPEG2000, MCIDAS, MPEG, MSP, PCD, PCX,
-                                  PIXAR, PNG, PPM, PSD, QOI, SGI, SPIDER, SUN, TGA, TIFF, WEBP, WMF, XBM, XPM, XVTHUMB
-
-    :param mode:    1 (1-bit pixels, black and white, stored with one pixel per byte)
-                    L (8-bit pixels, grayscale)
-                    P (8-bit pixels, mapped to any other mode using a color palette)
-                    RGB (3x8-bit pixels, true color)
-                    RGBA (4x8-bit pixels, true color with transparency mask)
-                    CMYK (4x8-bit pixels, color separation)
-                    YCbCr (3x8-bit pixels, color video format)
-                    Note that this refers to the JPEG, and not the ITU-R BT.2020, standard
-                    LAB (3x8-bit pixels, the L*a*b color space)
-                    HSV (3x8-bit pixels, Hue, Saturation, Value color space)
-                    Hue’s range of 0-255 is a scaled version of 0 degrees <= Hue < 360 degrees
-                    I (32-bit signed integer pixels)
-                    F (32-bit floating point pixels)
-
-    :param err_raise: if True, raise an error if the file is not found.
-    :param print_info: if True, print info about the progress.
-
+    :param: path:  path to the image file. format must be one of the accepted formats (see description).
+    :param: mode:  mode of the image (E.g. RGB, L, etc.. more info in the description).
+    :param: err_raise: if True, raise an error if the file is not found.
+    :param: print_info: if True, print info about the progress.
     :return: matrix of file in the given mode.
     """
     # check if file exists
@@ -56,26 +61,21 @@ def image_from_file(path, mode='RGB', err_raise=True, print_info=True):
     if not path:
         raise FileNotFoundError(f'Path: {path} not found!\n')
 
-    try:
-        with Image.open(path) as img:
-            if print_info:
-                print(f'Importing Success!\n')
-            img = img.convert(mode)
-            img = np.array(img)
-            return img
-
-    except Exception as e:
-        if err_raise:
-            print(f'Importing Failed!\n')
-            print(f'Error: {e}\n')
+    with Image.open(path) as img:
+        if print_info:
+            print(f'Importing Success!\n')
+        img = img.convert(mode)
+        img = np.array(img)
+        return img
 
 
 def images_from_folder(path, mode='RGB', err_raise=True, print_info=True):
     """Get images from a folder.
-    :param path: path to folder.
-    :param mode: mode of images.
-    :param err_raise: if True, raise an error if the file is not found.
-    :param print_info: if True, print info about the progress.
+
+    :param: path: path to folder.
+    :param: mode: mode of images.
+    :param: err_raise: if True, raise an error if the file is not found.
+    :param: print_info: if True, print info about the progress.
     :return: list of images.
     """
     # check if folder exists
@@ -88,8 +88,10 @@ def images_from_folder(path, mode='RGB', err_raise=True, print_info=True):
         print(f'Importing images from folder: {path}\n')
 
     for i in tqdm(os.listdir(path)):
-        lst.append(image_from_file(path + i, mode, err_raise, False))
-
+        try:
+            lst = np.append(lst, image_from_file(path + i, mode, err_raise, False))
+        except FileNotFoundError:
+            print(f"File not found! path: {path + i} \n")
     if print_info:
         print(f'Importing Success!\n')
 
@@ -100,8 +102,8 @@ def img_from_1d_array(arr, cols):
     """
     Create an 2D matrix representing a grayscale image from a 1D array.
 
-    :param cols: number of columns in the image.
-    :param arr: 1D array of pixels.
+    :param: cols: number of columns in the image.
+    :param: arr: 1D array of pixels.
     :return: matrix (image).
     """
     # validate input
@@ -119,20 +121,18 @@ def img_from_1d_array(arr, cols):
 def img_from_url(link, mode='RGB', err_raise=True, print_info=True):
     """
     Import an image from url.
-    :param link: link to image.
-    :param mode: mode of image.
-    :param err_raise: if True, raise an error if the file is not found.
-    :param print_info: if True, print info about the progress.
+
+    :param: link: link to image.
+    :param: mode: mode of image.
+    :param: err_raise: if True, raise an error if the file is not found.
+    :param: print_info: if True, print info about the progress.
     :return: matrix of image.
     """
     # check if link exists
     if not link:
         raise FileNotFoundError(f'Link: {link} not found!\n')
-    try:
-        urllib.request.urlretrieve(link, 'temp.jpg')
-    except Exception as e:
-        if err_raise:
-            print(f'Error: {e}\n')
+
+    urllib.request.urlretrieve(link, 'temp.jpg')
 
     return image_from_file('temp.jpg', mode, err_raise, print_info)
 
--- tests/image_import_test.py
+++ tests/image_import_test.py
@@ -1,63 +1,94 @@
+"""
+image_import_test.py
+======================
+
+Description:
+------------
+Tests the image.py module.
+
+Author:
+-------
+Yuval Shaffir
+
+Date:
+-----
+13/08/2023
+"""
+
 import unittest
 import os
 import numpy as np
 from PIL import Image
+import PIL
 import src.ImProUtils.image as ImProImage
 
 # Constants:
-
 # Test 1
 legal_file_path1 = os.path.abspath('test_images\\pizza_pixel_art.jpg')
 legal_mode1 = 'RGB'
-
 # Test 3
 illegal_file_type = os.path.abspath('test_images\\pizza_recipes.pdf')
 
 
-def test_image_from_legal_file():
-    """Test 1: image_from_file() with legal arguments."""
-    try:
+class TestFileImportation(unittest.TestCase):
+
+    def test_image_from_legal_file(self):
+        """Test 1: image_from_file() with legal arguments."""
+
         # RGB image:
         img = ImProImage.image_from_file(legal_file_path1, 'RGB')
-        assert isinstance(img, np.ndarray)
-        assert img.shape[2] == 3
-        assert img.shape[0] == 1080
-        assert img.shape[1] == 1000
-        assert img.dtype == np.uint8
+        self.assertIsInstance(img, np.ndarray)
+        self.assertEqual(img.shape[2], 3)
+        self.assertEqual(img.shape[0], 1080)
+        self.assertEqual(img.shape[1], 1000)
+        self.assertEqual(img.dtype, np.uint8)
 
         # grayscale image:
         img = ImProImage.image_from_file(legal_file_path1, 'L')
-        assert isinstance(img, np.ndarray)
-        assert img.shape[0] == 1080
-        assert img.shape[1] == 1000
-        assert img.dtype == np.uint8
-    except Exception as e:
-        assert False, f'Unexpected error: {e}'
+        self.assertIsInstance(img, np.ndarray)
+        self.assertEqual(img.shape[0], 1080)
+        self.assertEqual(img.shape[1], 1000)
+        self.assertEqual(img.dtype, np.uint8)
+
+    def test_image_from_illegal_file(self):
+        with self.assertRaises(IOError):
+            ImProImage.image_from_file('illegal_file_path', 'RGB')
+
+    def test_image_from_illegal_type(self):
+        with self.assertRaises(PIL.UnidentifiedImageError):
+            ImProImage.image_from_file(illegal_file_type, 'RGB')
+
+    def test_image_from_illegal_mode(self):
+        with self.assertRaises(ValueError):
+            ImProImage.image_from_file(legal_file_path1, 'illegal_mode')
 
 
-def test_image_from_illegal_file():
-    try:
-        img = ImProImage.image_from_file('illegal_file_path', 'RGB')
-    except FileNotFoundError:
-        pass
+class TestFolderImport(unittest.TestCase):
+    def test_legal_folder(self):
+        lst = ImProImage.images_from_folder('legal_img_folder\\')
+        self.assertIsInstance(lst, np.ndarray)
+        # Check the number of images in the folder is equal to the number of images imported:
+        self.assertEqual(len(lst), len([name for name in os.listdir('legal_img_folder\\') if os.path.isfile(name)]))
+        # Check that all images are of type np.ndarray:
+        for img in lst:
+            self.assertIsInstance(img, np.ndarray)
+
+    def test_illegal_folder_path(self):
+        with self.assertRaises(FileNotFoundError):
+            ImProImage.images_from_folder('illegal_folder_path\\')
+
+    def test_illegal_file_inside_folder(self):
+        with self.assertRaises(PIL.UnidentifiedImageError):
+            ImProImage.images_from_folder('test_images\\')
 
 
-def test_image_from_illegal_type():
-    try:
-        img = ImProImage.image_from_file(illegal_file_type, 'RGB')
-    except FileNotFoundError:
-        pass
+class Test1DArrayToImage(unittest.TestCase):
+    pass
 
 
-def test_image_from_illegal_mode():
-    try:
-        img = ImProImage.image_from_file(legal_file_path1, 'illegal_mode')
-    except ValueError:
-        pass
+class TestImageFromUrl(unittest.TestCase):
+    pass
 
 
 if __name__ == '__main__':
-    test_image_from_legal_file()
-    test_image_from_illegal_file()
-    test_image_from_illegal_type()
-    test_image_from_illegal_mode()
+    unittest.main()
