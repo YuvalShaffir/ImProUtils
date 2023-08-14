52cc9b2b0bd7a60083f39353f95a840fd18a674d
YuvalShaffir <34415892+YuvalShaffir@users.noreply.github.com>
2023-08-14 18:44:02 +0300

    Writing img_from_1d_array img_from_url

--- src/ImProUtils/image/image.py
+++ src/ImProUtils/image/image.py
@@ -20,6 +20,7 @@ import os
 from tqdm import tqdm
 from PIL import Image
 import numpy as np
+import urllib.request
 
 
 def image_from_file(path, mode='RGB', err_raise=True, print_info=True):
@@ -70,13 +71,19 @@ def image_from_file(path, mode='RGB', err_raise=True, print_info=True):
 
 
 def images_from_folder(path, mode='RGB', err_raise=True, print_info=True):
-    """Us threads to import images from a folder."""
+    """Get images from a folder.
+    :param path: path to folder.
+    :param mode: mode of images.
+    :param err_raise: if True, raise an error if the file is not found.
+    :param print_info: if True, print info about the progress.
+    :return: list of images.
+    """
     # check if folder exists
     path = os.path.abspath(path)
     if not path:
         raise FileNotFoundError(f'Path: {path} not found!\n')
 
-    lst = []
+    lst = np.array([])
     if print_info:
         print(f'Importing images from folder: {path}\n')
 
@@ -89,15 +96,34 @@ def images_from_folder(path, mode='RGB', err_raise=True, print_info=True):
     return lst
 
 
-def img_from_1d_array(arr):
+def img_from_1d_array(arr, cols):
     """
-    Create an 2D matrix representing an image from a 1D array.
+    Create an 2D matrix representing a grayscale image from a 1D array.
 
+    :param cols: number of columns in the image.
     :param arr: 1D array of pixels.
     :return: matrix (image).
     """
-    for i in range(len(arr) // )
+    # validate input
+    if not arr:
+        raise ValueError(f'Array is null!\n')
+    if cols < 1:
+        raise ValueError(f'Number of columns must be positive!\n')
+    if not isinstance(arr, list):
+        raise TypeError(f'Array must be a list!\n')
+
+    # reshape array into a matrix with the given number of columns
+    return np.reshape(arr, (-1, cols))
 
 
-def img_from_url():
-    pass
+def img_from_url(link, mode='RGB', err_raise=True, print_info=True):
+    """
+    Import an image from a url.
+    :param link:
+    :param mode:
+    :param err_raise:
+    :param print_info:
+    :return:
+    """
+    urllib.request.urlretrieve(link, 'temp.jpg')
+    return image_from_file('temp.jpg', mode, err_raise, print_info)
